7c3b1d96d86ad2e3ec1f4eb0125bac6ff01686f2
sdumaoziqi <15866605371@163.com>
2018-12-14 15:40:18 +0800

    generate random write to random.txt

--- contracts/random/gen_hash.cpp
+++ contracts/random/gen_hash.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <string>
 
-uint64_t HASH_STRING_PIECE(std::string string_piece) {                 
-    uint64_t result = 0;                                                        
+std::string HASH_STRING_PIECE(std::string string_piece) {
+    int64_t result = 0;
     for (auto it = string_piece.cbegin(); it != string_piece.cend(); ++it) {  
-        result = (result * 131) + *it;                                       
+        result = (result * 131) + *it;
     }                                                                         
-    return result;
+    return std::to_string(result);
 }
+
 
 int main(int argc, char *argv[])
 {
-	uint64_t  n = HASH_STRING_PIECE(argv[1]);
+	std::string  n = HASH_STRING_PIECE(argv[1]);
 	std::cout << n << std::endl;
 
 	return 0;
--- contracts/random/gen_random.py
+++ contracts/random/gen_random.py
@@ -8,9 +8,12 @@ import re
 import subprocess
 import sys
 import time
+import threading
 
 args = None
 logFile = None
+randomFile = None
+
 
 
 producerAccounts = [
@@ -66,11 +69,38 @@ def runGetOutput(args):
     return res
 
 def getHash(value):
-    res = runGetOutput("./gen_hash %d" % (value))
+    res = runGetOutput("./gen_hash %s" % (value))
     return res
 
 def getRandom():
     return numpy.random.randint(100000000)
+
+
+def getTable(term):
+    res = runGetOutput(args.cleos + 'get table zmaozmaozmao %d randoms' %(term))
+    j = json.loads(res)
+    value_count = j['rows'][0]['value_count']
+    hash_count = j['rows'][0]['hash_count']
+    if hash_count > 0 and hash_count == value_count:
+        value = ""
+        i = 0
+        while i < j['rows'][0]['hash_count']:
+            value += j['rows'][0]['producers_hash'][i]['value']
+            i += 1
+        hash = getHash(value)
+        randomFile.write('%s  term #%02d  %s \n' %(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()), term, hash))
+        print('term #', term, ' hash:', hash)
+    
+
+class calRandom(threading.Thread):
+    def __init__(_self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        sleep(15)
+        print('开始计算Random')
+
 
 
 # Command Line Arguments
@@ -86,6 +116,8 @@ parser.add_argument('--contracts-dir', metavar='', help="Path to contracts direc
 parser.add_argument('--nodes-dir', metavar='', help="Path to nodes directory", default='./nodes/')
 parser.add_argument('--wallet-dir', metavar='', help="Path to wallet directory", default='./wallet/')
 parser.add_argument('--log-path', metavar='', help="Path to log file", default='./gen_random_log.log')
+parser.add_argument('--random-path', metavar='', help="Path to random file", default='./random.txt')
+
 parser.add_argument('-a', '--all', action='store_true', help="Do everything marked with (*)")
 parser.add_argument('-H', '--http-port', type=int, default=8000, metavar='', help='HTTP port for cleos')
 
@@ -96,16 +128,25 @@ args = parser.parse_args()
 args.cleos += '--url http://127.0.0.1:%d ' % args.http_port
 
 logFile = open(args.log_path, 'a')
+randomFile = open(args.random_path, 'a')
 
-logFile.write('\n\n' + '*' * 80 + '\n\n\n')
+
+logFile.write('\n\n' + '*' * 80 + '\n\n')
+randomFile.write('\n' + '*' * 80 + '\n')
+
 
 
 term = 1
 
 
-dict = {'useraaaaaaaa': 1, 'useraaaaaaab': 2, 'useraaaaaaac': 3}
+dict = {'useraaaaaaaa': "1", 'useraaaaaaab': "2", 'useraaaaaaac': "3"}
 
 setContract()
+#getTable(1)
+
+
+
+
 
 while term < 10:
     print('*' * 60)
@@ -119,6 +160,7 @@ while term < 10:
     for producer in producerAccounts:
         pushValue(producer, term, str(dict[producer]))
         sleep(1)
+    getTable(term)
     term+=1
 
 
