cad454790d3f275fb271224daa499182202cf1f5
sdumaoziqi <15866605371@163.com>
2018-12-14 16:11:44 +0800

    add args in python script

--- contracts/random/gen_random.py
+++ contracts/random/gen_random.py
@@ -90,7 +90,7 @@ def getTable(term):
             value += j['rows'][0]['producers_hash'][i]['value']
             i += 1
         hash = getHash(value)
-        randomFile.write('%s  term #%02d  %25s \n' %(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()), term, hash))
+        randomFile.write('%s  term #%04d  %25s \n' %(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()), term, hash))
         print('term #', term, ' hash:', hash)
     
 
@@ -119,6 +119,7 @@ parser.add_argument('--nodes-dir', metavar='', help="Path to nodes directory", d
 parser.add_argument('--wallet-dir', metavar='', help="Path to wallet directory", default='./wallet/')
 parser.add_argument('--log-path', metavar='', help="Path to log file", default='./gen_random_log.log')
 parser.add_argument('--random-path', metavar='', help="Path to random file", default='./random.txt')
+parser.add_argument('-r', '--round', metavar='', help="num of random", default=5)
 
 parser.add_argument('-a', '--all', action='store_true', help="Do everything marked with (*)")
 parser.add_argument('-H', '--http-port', type=int, default=8000, metavar='', help='HTTP port for cleos')
@@ -138,31 +139,31 @@ randomFile.write('\n' + '*' * 80 + '\n')
 
 
 
-term = 1
+term = 0
 
 
 dict = {'useraaaaaaaa': "1", 'useraaaaaaab': "2", 'useraaaaaaac': "3"}
 
 setContract()
-#getTable(1)
 
 
 
 
+sleep(2)
 
-while term < 10:
+while term < int(args.round):
     print('*' * 60)
-    sleep(2)
     for producer in producerAccounts:
         random = getRandom()
         dict[producer] = random
         hash = getHash(random)
         pushHash(producer, term, hash)
-        sleep(1)
+    sleep(1)
     for producer in producerAccounts:
         pushValue(producer, term, str(dict[producer]))
-        sleep(1)
+    sleep(1)
     getTable(term)
     term+=1
+    sleep(1)
 
 
